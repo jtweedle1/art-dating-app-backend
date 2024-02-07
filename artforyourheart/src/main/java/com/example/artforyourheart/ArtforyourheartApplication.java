21c256c77bb78045dc93082d9dc96c8d8fcf8b5a
saraiesco <saraiescobar@google.com>
2024-02-07 14:47:03 -0600

    first commit

---
+++ artforyourheart/src/main/java/com/example/artforyourheart/ArtforyourheartApplication.java
@@ -0,0 +1,13 @@
+package com.example.artforyourheart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtforyourheartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtforyourheartApplication.class, args);
+	}
+
+}
---
+++ artforyourheart/src/test/java/com/example/artforyourheart/ArtforyourheartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artforyourheart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtforyourheartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
