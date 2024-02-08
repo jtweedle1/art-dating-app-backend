46cde503c3d615a427ed0562236f34a73a4f2968
jrtweedle1 <tweedle@google.com>
2024-02-08 13:41:54 -0600

    adding database connection

---
+++ artforyourheart/src/main/java/com/example/artforyourheart/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.artforyourheart.repository;
+
+import com.example.artforyourheart.model.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+}
