7dec02e49881fe12b45fcf0a8cb21637a616c3ee
saraiesco <saraiescobar@google.com>
2024-02-20 13:23:44 -0600

    getAllUsers method

--- artforyourheart/src/main/java/com/example/artforyourheart/controller/UserController.java
+++ artforyourheart/src/main/java/com/example/artforyourheart/controller/UserController.java
@@ -1,14 +1,28 @@
 package com.example.artforyourheart.controller;
 
 
+import com.example.artforyourheart.model.User;
 import com.example.artforyourheart.repository.UserRepository;
+import com.example.artforyourheart.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
+
+
+    //get all
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<List<User>>(userService.allUsers(), HttpStatus.OK);
+    }
 }
--- artforyourheart/src/main/java/com/example/artforyourheart/service/UserService.java
+++ artforyourheart/src/main/java/com/example/artforyourheart/service/UserService.java
@@ -1,12 +1,20 @@
 package com.example.artforyourheart.service;
 
 
+import com.example.artforyourheart.model.User;
 import com.example.artforyourheart.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    public List<User> allUsers(){
+        System.out.println(userRepository.findAll());
+        return userRepository.findAll();
+    }
 }
